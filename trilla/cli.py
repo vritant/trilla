dac971d72c95161dc9d8f95e65785393afb51d01
Vritant Jain <adarshvritant@gmail.com>
2016-06-21 00:03:06 -0400

    Added commands per initial thoughts

--- trilla/cli.py
+++ trilla/cli.py
@@ -2,13 +2,55 @@
 
 import click
 
-@click.command()
-def main(args=None):
-    """Console script for trilla"""
-    click.echo("Replace this message by putting your code into "
-                "trilla.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+class Config(object):
+    def __init__(self):
+        self.profile = "default"
 
+pass_config = click.make_pass_decorator(Config, ensure = True)
+
+@click.group()
+@click.option('--profile','-p', default = "default", help="profile name")
+@pass_config
+def main(config, profile):
+    """Console script for trilla"""
+    config.profile = profile
+    pass
+
+@main.group()
+def track():
+    """Track entities"""
+
+@track.command()
+@pass_config
+def bugs(config):
+    """Track bugzillla bugs"""
+    click.echo("Profile %s" % config.profile)
+
+@track.command()
+@pass_config
+def prs(config):
+    """Track github PRs"""
+
+@track.command()
+@pass_config
+def issues(config):
+    """Track github issues"""
+
+@main.command()
+@click.option('--all-bugs', help="sync all tracked bugzilla bugs")
+@click.option('--all-prs', help="sync all tracked github PRs")
+@click.option('--all-issues', help="sync all tracked github issues")
+@click.option('--all', help="sync all tracked bugs, github issues and PRs")
+@pass_config
+def sync(config, all_bugs, all_prs, all_issues, all):
+    """Sync tracked entities"""
+    click.echo('Implement Sync command')
+
+@main.command()
+@pass_config
+def untrack(config):
+    """Untrack specific entities"""
+    click.echo('Implement untrack')
 
 if __name__ == "__main__":
     main()
